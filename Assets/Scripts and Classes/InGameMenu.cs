8135b027a97e2c0723e88d480c018fba8bc9a236
ZainV <drummerboyzain@rogers.com>
2016-03-13 14:43:13 -0400

    More inventory work done

--- Assets/Scripts and Classes/InGameMenu.cs
+++ Assets/Scripts and Classes/InGameMenu.cs
@@ -1,26 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[RequireComponent (typeof (InventoryGUI))]
 public class InGameMenu : MonoBehaviour
 {
     private Rect menuWindowRect = new Rect(Screen.width / 2 - 200, Screen.height / 2 - 300, 400, 600);
     private bool menuWindowToggle = false;
     private bool button1;
 
-
+    InventoryGUI userInv;
     // Use this for initialization
     void Start()
     {
-
+        userInv = GetComponent<InventoryGUI>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Escape) && menuWindowToggle == false)
         {
             menuWindowToggle = true;
-            OnGUI(); 
         }
     }
     void OnGUI()
@@ -28,15 +27,25 @@ public class InGameMenu : MonoBehaviour
         if (menuWindowToggle == true)
         {
             menuWindowRect = GUI.Window(0, menuWindowRect, MenuWindowMethod, "Menu");
-            if (Input.GetKey(KeyCode.Space) && menuWindowToggle == true)
+            if (button1)
             {
-                menuWindowToggle = false;
-                menuWindowRect = new Rect(0, 0, Screen.width, Screen.height);
+                userInv.SetToggle(true);
+
             }
+
         }
     }
     void MenuWindowMethod(int windowID)
     {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
 
+        GUILayout.BeginHorizontal();
+        button1 = GUILayout.Button("Inventory", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Crafting", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Journal", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Survival Guide", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.EndHorizontal();
+
+        GUILayout.EndArea();
     }
 }
--- Assets/Scripts and Classes/Inventory/InventoryGUI.cs
+++ Assets/Scripts and Classes/Inventory/InventoryGUI.cs
@@ -5,7 +5,10 @@ public class InventoryGUI : MonoBehaviour {
     private Rect inventoryWindowRect = new Rect(0, 0, Screen.width, Screen.height);
     private bool inventoryWindowToggle = false;
     private bool button1;
-
+    public void SetToggle(bool status)
+    {
+        inventoryWindowToggle = status;
+    }
     // Use this for initialization
     void Start () {
 	
@@ -16,7 +19,6 @@ public class InventoryGUI : MonoBehaviour {
         if (Input.GetKey(KeyCode.I))
         {
             inventoryWindowToggle = true;
-            OnGUI();
         }
     }
     void OnGUI()
@@ -33,66 +35,66 @@ public class InventoryGUI : MonoBehaviour {
         }
     }
     void InventoryWindowMethod(int windowID){
-        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+        GUILayout.BeginArea(new Rect(0, 20, Screen.width, Screen.height - 20));
 
         GUILayout.BeginHorizontal();
-        GUILayout.Button("Item 1", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 2", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 3", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 4", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 5", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 6", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 7", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 8", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 9", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Item 1", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 2", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 3", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 4", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 5", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 6", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 7", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 8", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 9", GUILayout.Height((Screen.height - 40) / 6));
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
-        GUILayout.Button("Item 10", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 11", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 12", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 13", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 14", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 15", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 16", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 17", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 18", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Item 10", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 11", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 12", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 13", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 14", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 15", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 16", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 17", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 18", GUILayout.Height((Screen.height - 40) / 6));
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
-        GUILayout.Button("Item 19", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 20", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 21", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 22", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 23", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 24", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 25", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 26", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 27", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Item 19", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 20", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 21", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 22", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 23", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 24", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 25", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 26", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 27", GUILayout.Height((Screen.height - 40) / 6));
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
-        GUILayout.Button("Item 28", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 29", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 30", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 31", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 32", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 33", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 34", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 35", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 36", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Item 28", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 29", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 30", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 31", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 32", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 33", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 34", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 35", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 36", GUILayout.Height((Screen.height - 40) / 6));
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
-        GUILayout.Button("Item 37", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 38", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 39", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 40", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 41", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 42", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 43", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 44", GUILayout.Height(Screen.height / 5 - 200));
-        GUILayout.Button("Item 45", GUILayout.Height(Screen.height / 5 - 200));
+        GUILayout.Button("Item 37", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 38", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 39", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 40", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 41", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 42", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 43", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 44", GUILayout.Height((Screen.height - 40) / 6));
+        GUILayout.Button("Item 45", GUILayout.Height((Screen.height - 40) / 6));
         GUILayout.EndHorizontal();
 
         GUILayout.EndArea();
